75aa4b8056c438b1d6225fe8a401f14086dcce67
Josafat Solis <jsolis@macroaplicaciones.com>
2020-05-28 18:41:43 -0500

    Funciones adicionales

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,79 +1,142 @@
 const express = require("express");
 const router = express.Router();
 const Ticket = require("../models/Ticket");
-const {verifyToken} = require("../utils/verifyToken");
+const { verifyToken } = require("../utils/verifyToken");
 const { checkRole } = require("../utils/checkRole");
 
-//ROUTE GET ALL
-router.get("/", verifyToken, checkRole(["Tecnician", "Admin"]), function (req, res, next) {
-    Ticket.find()
-      .then((tickets) => res.status(200).json(tickets))
+// REGRESA TDOOS
+router.get("/", verifyToken, checkRole(["Tecnician", "Admin"]), function (
+  req,
+  res,
+  next
+) {
+  Ticket.find()
+    .then((tickets) => res.status(200).json(tickets))
+    .catch((reason) => {
+      console.log("Error: ", reason);
+      res.status(400).json({ error: reason });
+    });
+});
+
+// REGRESA TODOS LOS DE UN TENANT_ID
+router.get(
+  "/bytenant/:tenant_id",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
+    const { tenant_id } = req.params;
+    Ticket.find({ tenant: tenant_id })
+    .populate("clientUser", "email name lastName")
+    .populate("tecnicianUser", "email name lastName")
+      .then((found) => {
+        if (found) {
+          res.status(200).json(found);
+        } else {
+          res.status(200).json({});
+        }
+      })
+      .catch((reason) => {
+        // O que ocurra un error si el id no viene en el formato correcto.
+        console.log("Error: ", reason);
+        res.status(404).json({ error: reason });
+      });
+  }
+);
+
+router.get(
+  "/bytenant/:tenant_id/open",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
+    const { tenant_id } = req.params;
+    Ticket.find({ tenant: tenant_id, status: "Open" })
+      .then((found) => {
+        if (found) {
+          res.status(200).json(found);
+        } else {
+          res.status(200).json({});
+        }
+      })
+      .catch((reason) => {
+        // O que ocurra un error si el id no viene en el formato correcto.
+        console.log("Error: ", reason);
+        res.status(404).json({ error: reason });
+      });
+  }
+);
+
+//ROUTE GET ID
+router.get(
+  "/:id",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
+    const { id } = req.params;
+    Ticket.findById(id)
+      .then((found) => {
+        if (found) {
+          res.status(200).json(found);
+        } else {
+          // Pueden darse ambos casos, que no encuentre..
+          console.log("Empty.....");
+          res.status(404).json({});
+        }
+      })
+      .catch((reason) => {
+        // O que ocurra un error si el id no viene en el formato correcto.
+        console.log("Error: ", reason);
+        res.status(404).json({ error: reason });
+      });
+  }
+);
+
+//CREAR TICKET
+router.post(
+  "/",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
+    console.log(req.body);
+    Ticket.create(req.body)
+      .then((created) => {
+        res.status(200).json({ created });
+        console.log(created);
+      })
       .catch((reason) => {
         console.log("Error: ", reason);
         res.status(400).json({ error: reason });
       });
-  });
+  }
+);
 
-//ROUTE GET TENANT_ID
-router.get("/bytenant/:tenant_id", verifyToken, checkRole(["User", "Tecnician", "Admin"]), (req, res, next) => {
-  const { tenant_id } = req.params;
-  Ticket.find({tenant: tenant_id}).then(found => {
-      if (found) {
-          res.status(200).json(found);
-      } else {
-          res.status(200).json({});
-      }
-  }).catch(reason => {
-      // O que ocurra un error si el id no viene en el formato correcto.
-      console.log("Error: ", reason);
-      res.status(404).json({error: reason});
-  })
-});
-
-router.get("/bytenant/:tenant_id/open", verifyToken, checkRole(["User", "Tecnician", "Admin"]), (req, res, next) => {
-  const { tenant_id } = req.params;
-  Ticket.find({tenant: tenant_id, status: "Open"}).then(found => {
-      if (found) {
-          res.status(200).json(found);
-      } else {
-          res.status(200).json({});
-      }
-  }).catch(reason => {
-      // O que ocurra un error si el id no viene en el formato correcto.
-      console.log("Error: ", reason);
-      res.status(404).json({error: reason});
-  })
-});
-
-//ROUTE GET ID
-router.get("/:id", verifyToken, checkRole(["User", "Tecnician", "Admin"]), (req, res, next) => {
+// ASIGNAR UN TICKET A UN TECNICIAN
+router.patch(
+  "/:id",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
     const { id } = req.params;
-    Ticket.findById(id).then(found => {
-        if (found) {
-            res.status(200).json(found);
+    const { tecnicianUser } = req.body;
+    // Note that new returns the updated version
+    Ticket.findByIdAndUpdate(id, {tecnicianUser}, { new: true })
+      .then((updated) => {
+        if (updated) {
+          res.status(200).json(updated);
         } else {
-            // Pueden darse ambos casos, que no encuentre..
-            console.log("Empty.....");
-            res.status(404).json({});
+          console.log("Error: Updated fail, not found.");
+          res.status(404).json({});
         }
-    }).catch(reason => {
-        // O que ocurra un error si el id no viene en el formato correcto.
-        console.log("Error: ", reason);
-        res.status(404).json({error: reason});
-    })
-});
-  
-//ROUTE POST
-router.post("/", verifyToken, checkRole(["User", "Tecnician", "Admin"]), (req, res, next) => {
-  Ticket.create(req.body)
-    .then((created) => {res.status(200).json({ created });
-    console.log(created);
-})
-    .catch((reason) => res.status(400).json({ error: reason }));
-});
+      })
+      .catch((reason) => res.status(400).json({ error: reason }));
+  }
+);
 
 //ROUTE UPDATE
-router.patch("/:id", verifyToken, checkRole(["User", "Tecnician", "Admin"]), (req, res, next) => {
+router.patch(
+  "/:id",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
     const { id } = req.params;
     // Note that new returns the updated version
     Ticket.findByIdAndUpdate(id, req.body, { new: true })
@@ -86,14 +149,15 @@ router.patch("/:id", verifyToken, checkRole(["User", "Tecnician", "Admin"]), (re
         }
       })
       .catch((reason) => res.status(400).json({ error: reason }));
-  });
-  
-  //ROUTE DELETE
-  router.delete("/:id", verifyToken, checkRole(["Admin"]), (req, res, next) => {
-    const { id } = req.params;
-    Ticket.findByIdAndDelete(id)
-      .then((deleted) => res.status(200).json({ deleted }))
-      .catch((reason) => res.status(400).json({ error: reason }));
-  });
-  
+  }
+);
+
+//ROUTE DELETE
+router.delete("/:id", verifyToken, checkRole(["Admin"]), (req, res, next) => {
+  const { id } = req.params;
+  Ticket.findByIdAndDelete(id)
+    .then((deleted) => res.status(200).json({ deleted }))
+    .catch((reason) => res.status(400).json({ error: reason }));
+});
+
 module.exports = router;
