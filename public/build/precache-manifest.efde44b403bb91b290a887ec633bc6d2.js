3bea698dbb2f356e83c068be55ef11345223065f
Josafat Solis <jsolis@macroaplicaciones.com>
2020-05-29 20:22:20 -0500

    Test

---
+++ public/build/precache-manifest.efde44b403bb91b290a887ec633bc6d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caec66ce0faeb870ab4f7372d4a47f86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af022486f6ffe37dd9c9",
+    "url": "/static/css/2.43de3167.chunk.css"
+  },
+  {
+    "revision": "e867aa32abdc01661108",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af022486f6ffe37dd9c9",
+    "url": "/static/js/2.29f51b93.chunk.js"
+  },
+  {
+    "revision": "1b6acbe1f09af4ae6aff1c4a3f5ecf7e",
+    "url": "/static/js/2.29f51b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e867aa32abdc01661108",
+    "url": "/static/js/main.a436b14f.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "a712b5619b96939a7acbc803a7b47cb0",
+    "url": "/static/media/logo.a712b561.png"
+  },
+  {
+    "revision": "94fe1a7ade7da7e3843bf0ce59b42d09",
+    "url": "/static/media/playa.94fe1a7a.jpg"
+  }
+]);
