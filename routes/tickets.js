a1da7b49710c1c58175b2633389b9b0e29cbc934
Josafat Solis <jsolis@macroaplicaciones.com>
2020-05-29 19:59:20 -0500

    Version candidata para presentación 1

--- routes/tickets.js
+++ routes/tickets.js
@@ -26,8 +26,8 @@ router.get(
   (req, res, next) => {
     const { tenant_id } = req.params;
     Ticket.find({ tenant: tenant_id })
-    .populate("clientUser", "email name lastName")
-    .populate("tecnicianUser", "email name lastName")
+      .populate("clientUser", "email name lastName")
+      .populate("tecnicianUser", "email name lastName")
       .then((found) => {
         if (found) {
           res.status(200).json(found);
@@ -43,6 +43,7 @@ router.get(
   }
 );
 
+// ++ EN PROCESO ++ REGRESAR TODOS LOS TICKETS ABIERTOS DE UN TENANT
 router.get(
   "/bytenant/:tenant_id/open",
   verifyToken,
@@ -118,7 +119,7 @@ router.patch(
     const { id } = req.params;
     const { tecnicianUser } = req.body;
     // Note that new returns the updated version
-    Ticket.findByIdAndUpdate(id, {tecnicianUser}, { new: true })
+    Ticket.findByIdAndUpdate(id, { tecnicianUser }, { new: true })
       .then((updated) => {
         if (updated) {
           res.status(200).json(updated);
@@ -128,6 +129,74 @@ router.patch(
         }
       })
       .catch((reason) => res.status(400).json({ error: reason }));
+  }
+);
+
+// AGREGAR UNA ACTIVIDAD A UN TICKET
+router.post(
+  "/:id/newactivity",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
+    const { id } = req.params;
+    Ticket.findById(id)
+      .then((ticket) => {
+        if (ticket) {
+          const current = ticket.activities;
+          current.push(req.body);
+          return current;
+        }
+      })
+      .then((updatedActivities) => {
+        if (updatedActivities) {
+          Ticket.findByIdAndUpdate(
+            id,
+            { activities: updatedActivities },
+            { new: true }
+          )
+            .then((updated) => {
+              if (updated) {
+                res.status(200).json(updated);
+              } else {
+                console.log("Error: Updated fail, not found.");
+                res.status(404).json({});
+              }
+            })
+            .catch((reason) => {
+              console.log("Error: ", reason);
+              res.status(400).json({ error: reason });
+          });
+        }
+      })
+      .catch((reason) => {
+        console.log("Error: ", reason);
+        res.status(400).json({ error: reason });
+      });
+  }
+);
+
+// CAMBIAR EL ESTADO A UN TICKET
+router.patch(
+  "/:id/statusupdate",
+  verifyToken,
+  checkRole(["User", "Tecnician", "Admin"]),
+  (req, res, next) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    // Note that new returns the updated version
+    Ticket.findByIdAndUpdate(id, { status }, { new: true })
+      .then((updated) => {
+        if (updated) {
+          res.status(200).json(updated);
+        } else {
+          console.log("Error: Updated fail, not found.");
+          res.status(404).json({});
+        }
+      })
+      .catch((reason) => {
+        res.status(400).json({ error: reason });
+        console.log("Error: ", reason);
+      });
   }
 );
 
